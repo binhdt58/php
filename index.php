854a68b9243d7eef41f6e4cc510cb083958cfa29
Binh Dang <binhdangtran21@gmail.com>
2016-01-24 09:57:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        // put your code here
+        echo "<font color=red face=tahoma size=20>Welcome to VietNam <font>";
+        ?>
+    </body>
+</html>
