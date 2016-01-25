714441f8dcae68045746107652f907cc0d73752e
Binh Dang <binhdangtran21@gmail.com>
2016-01-25 09:27:06 +0700

    ne

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ and open the template in the editor.
         <?php
         // put your code here
 		
-        echo "<font color=green face=tahoma size=20>Ho ten: Dang Tran Binh  <font> </br>";
-		echo "<font color=green face=tahoma size=20>Ngay sinh : 21-10-1995 <font> </br>";
-		echo "<font color=green face=tahoma size=20>Dia chi: Ha Noi <font> </br>";
-		echo "<font color=green face=tahoma size=20>So thich: doc truyen tranh , nghe nhac ^^  <font> </br>";
-		echo "<font color=red face=tahoma size=20>Doi net ve ban than: Sinh vien nam 3 Truong Dai hoc Cong Nghe- DHQGHN. Than hinh hoi map, chua duoc cao, hien lanh, de bao, it noi :))) <font>";
+        echo "<font color=green face=tahoma size=10>Ho ten: Dang Tran Binh  <font> </br>";
+		echo "<font color=green face=tahoma size=10>Ngay sinh : 21-10-1995 <font> </br>";
+		echo "<font color=green face=tahoma size=10>Dia chi: Ha Noi <font> </br>";
+		echo "<font color=green face=tahoma size=10>So thich: doc truyen tranh , nghe nhac ^^  <font> </br>";
+		echo "<font color=green face=tahoma size=10>Doi net ve ban than: Sinh vien nam 3 Truong Dai hoc Cong Nghe- DHQGHN. Than hinh hoi map, chua duoc cao, hien lanh, de bao, it noi :))) <font>";
         ?>
-		<img src="1.jpg" width="960" height="720" alt="1"/>
+		<img src="DSC00319.JPG" width="60" height="92" alt="1"/>
     </body>
 </html>
