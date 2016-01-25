5e6a30d9f1c410658cf6fd4d0e857833624bcd4f
Binh Dang <binhdangtran21@gmail.com>
2016-01-25 09:08:13 +0700

    new

--- index.php
+++ index.php
@@ -13,7 +13,13 @@ and open the template in the editor.
         
         <?php
         // put your code here
-        echo "<font color=red face=tahoma size=20>Ho ten: Dang Tran Binh \n Ngay sinh : 21-10-1995 \n Dia chi: Ha Noi So thich: doc truyen tranh , nghe nhac ^^  <font>";
+		
+        echo "<font color=green face=tahoma size=20>Ho ten: Dang Tran Binh  <font>";
+		echo "<font color=green face=tahoma size=20>Ngay sinh : 21-10-1995 <font>";
+		echo "<font color=green face=tahoma size=20>Dia chi: Ha Noi <font>";
+		echo "<font color=green face=tahoma size=20>So thich: doc truyen tranh , nghe nhac ^^  <font>";
+		echo "<font color=red face=tahoma size=20>Doi net ve ban than: Sinh vien nam 3 Truong Dai hoc Cong Nghe- DHQGHN. Than hinh hoi map, chua duoc cao, hien lanh, de bao, it noi :))) <font>";
         ?>
+		<img src="1.jpg" width="960" height="720" alt="123"/>
     </body>
 </html>
