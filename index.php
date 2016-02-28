2e469302b417b61c93be307bb53adac967455e1a
Binh Dang <binhdangtran21@gmail.com>
2016-02-28 08:48:53 +0700

    commit ex week1

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ and open the template in the editor.
         <title>Thong tin ca nhan</title>
     </head>
     <body>
+        <h1> Information </h1>
+        <div class = 'info'>
         <table cellpadding = "5" width = "1000">
 			<tr><td>
 				<img src = "DSC00319.JPG" align = "left" width = 200>
@@ -19,8 +21,13 @@ and open the template in the editor.
 				<p><b>Dia chi : </b> Ha Noi  <br> 
 				<p><b>So thich :</b>  doc truyen tranh , nghe nhac ^^  <br> 
 				<p><b>Doi net ve ban than :</b> Sinh vien nam 3 Truong Dai hoc Cong Nghe- DHQGHN. Than hinh hoi map, chua duoc cao, hien lanh, de bao, it noi :)))  <br> 
-
-		
+                          
+        
+                                    </div>
+        <h2> Exercises </h2>
+            <ul>
+                <li><h3>7 Days of JavaScript Week 1: <small><a href="/image/ex.html">Click to view!</a></small></h3></li>
+ 	    </ul>
 		
 		
 		
